a0b405ec2ac8919deb5f4fa16dfb7fe0547e9f3b
Almintas <almintasbruzas@gmail.com>
2022-11-15 22:05:12 +0200

    Developing register page

--- src/Pages/Register/RegisterPage.jsx
+++ src/Pages/Register/RegisterPage.jsx
@@ -1,18 +1,38 @@
 import { RegisterButton } from "../../Components/Buttons/RegisterButton/RegisterButton";
-import { FormWrapper, InputWrapper } from "./RegisterPage.styled";
+import { FormWrapper, InputWrapperEmail, InputWrapperPassword } from "./RegisterPage.styled";
 
 
 export const RegisterPage = () => {
     const handleRegisterButton = (event) => {
         event.preventDefault();
-    }
 
-    return (
-        <FormWrapper onSubmit={handleRegisterButton}>
-            <InputWrapper type='email' placeholder='Email' />
-            <InputWrapper type='password' placeholder='Password' />
-            <br />
-            <RegisterButton />
-        </FormWrapper>
-    )
-}
+        const BASE_URL = ('https://autumn-delicate-wilderness.glitch.me/v1/');
+
+        fetch(BASE_URL + 'auth/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: '',
+                password: 'kikitata'
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+};
+
+
+
+
+return (
+    <FormWrapper onSubmit={handleRegisterButton}>
+        <InputWrapperEmail type='email' placeholder='Email' />
+        <InputWrapperPassword type='password' placeholder='Password' />
+        <br />
+        <RegisterButton />
+    </FormWrapper>
+)
+};
--- src/Pages/Register/RegisterPage.styled.js
+++ src/Pages/Register/RegisterPage.styled.js
@@ -5,7 +5,12 @@ export const FormWrapper = styled.form`
 margin-top: 50px;
 `;
 
-export const InputWrapper = styled.input`
+export const InputWrapperEmail = styled.input`
+margin-right: 15px;
+border-radius: 10px;
+`;
+
+export const InputWrapperPassword = styled.input`
 margin-right: 15px;
 border-radius: 10px;
 `;
